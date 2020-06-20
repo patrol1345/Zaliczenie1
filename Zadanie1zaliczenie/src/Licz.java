0322ec106911614ea7caab24ea0e585c783079ef
patrol1345 <63158468+patrol1345@users.noreply.github.com>
2020-06-20 10:08:11 +0200

    Add files via upload

---
+++ Zadanie1zaliczenie/src/Licz.java
@@ -0,0 +1,17 @@
+public class Licz {
+    public static void main(String[] args) {
+
+	  int a = 12;
+      int b = 4;
+      int c = a + b;
+      System.out.println(c+"=12+4");
+      c = a - b;
+      System.out.println(c+"=12-4");
+      c = a * b;
+      System.out.println(c+"=12*4");
+      c = a / b;
+      System.out.println(c+"=12/4");
+      c = a % b;
+      System.out.println(c+"=12%4");
+}
+}
